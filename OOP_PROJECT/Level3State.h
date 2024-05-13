a38d5ef4122fcc8ef31e1383c8afd5d39e19cca3
Muhammadisam-13 <156660076+Muhammadisam-13@users.noreply.github.com>
2024-05-13 05:43:34 +0500

    Level 3 and 4, and selection state

---
+++ OOP_PROJECT/Level3State.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "StateMachine.h"
+#include "PauseGameState.h"
+#include "Sun.h"
+#include "Plant.h"
+#include "PeaShooter.h"
+#include "PlantFactory.h"
+#include <cstdlib>
+#include "ZombieFactory.h"
+#include "LawnMower.h"
+using namespace std;
+
+class Level3State : public State
+{
+private:
+	// UI Sprites
+	sf::Sprite background;
+	sf::Sprite seedPacketSprite;
+	sf::Clock ZombieSpawnRate;  // Used to spawn zombies
+	sf::Sprite shovelSprite;
+	sf::Text elapsedTimeText;
+	// UI features
+	sf::Clock ElapsedTime;
+	sf::Font font;
+	sf::Text sunsNumText;
+	sf::Clock sunClock;
+	sf::Clock sunflowerSunClock;
+	sf::Text livesText;
+
+	// Sounds
+	sf::SoundBuffer sungen_SoundBuffer;
+	sf::Sound sungen_Sound;
+	sf::SoundBuffer placingPlantSoundBuffer;
+	sf::Sound zombieWave;
+	sf::SoundBuffer zombieWaveBuffer;
+	sf::Sound placingPlantSound;
+	sf::Music backgroundMusic;
+	sf::IntRect textureRect;
+
+	// Game Objects
+	PlantFactory AllPlants;
+	ZombieFactory AllZombies;
+	Sun* suns;
+	LawnMower* lawnmowers;
+
+	// Numbers for loops
+	const int MAX_lawnmowers = 5;
+	const int AllZombieTypes = 5;
+
+
+	// Required variables
+	int Lives;
+	bool FIELD_GAME_STATUS[5][9];  // The status of the game field
+	bool clickedSeedPacket[4]; // change size later
+	bool clickedShovel;
+	int totalTypeInstancesCreated = 1;
+	int sunCount;
+	int currentSuns = 0;
+	int sunsNum;
+	int totalShooterInstancesCreated = 1;
+	int totalNormalPlantInstancesCreated = 1;
+	bool leftMouseButtonPressed = false;
+	const int totalPlantTypes = 4;
+	int sunflowerInstances = 0;
+
+
+public:
+	Level3State();
+private:
+	void setPlantFactoryTextures(AssetManager& Assets);
+	void setZombieTextures(AssetManager& Assets);
+	void setUITextures(AssetManager& Assets);
+	void handleAllPlantsCreation(sf::RenderWindow& window);
+	void spawnZombies();  // Spawn the zombies
+	void renderPlantFactory(sf::RenderWindow& window);
+	void renderZombies(sf::RenderWindow& window, float deltaTime);
+	void renderUI(sf::RenderWindow& window);
+	void generateSuns(sf::RenderWindow& window, float deltaTime);
+	void handleSunCollection(sf::RenderWindow& window);
+	void handlePlantRemoval(sf::RenderWindow& window);
+	void handleBulletZombieCollision(sf::RenderWindow& window);
+	void handlePlantZombieCollision();
+	void handleLawnMowerCollision(float deltaTime);
+	virtual void Init(AssetManager& Assets);
+	virtual void HandleInput(StateMachine* machine, sf::RenderWindow& window);
+	virtual void Update(StateMachine* machine, float deltaTime);
+	virtual void Draw(sf::RenderWindow& window, float deltaTime);
+};
--- OOP_PROJECT/LevelSelectionState.cpp
+++ OOP_PROJECT/LevelSelectionState.cpp
@@ -21,6 +21,44 @@ void LevelSelectionState::HandleInput(StateMachine* machine, sf::RenderWindow& w
 
 			machine->AddState(StateRef(new Level1State));  // Replacing the meny state with the level 1 state
 		}
+
+		//if (mousePosition.x >= 111 && mousePosition.x <= 598 && mousePosition.y >= 272 && mousePosition.y <= 368)
+		//{
+		//	machine->RemoveState();  // Going back to the main menu
+
+		//	machine->AddState(StateRef(new Level2State));  // Replacing the meny state with the level 1 state
+		//}
+
+		if (mousePosition.x >= 111 && mousePosition.x <= 598 && mousePosition.y >= 430 && mousePosition.y <= 555)
+		{
+			machine->RemoveState();  // Going back to the main menu
+
+			machine->AddState(StateRef(new Level3State));  // Replacing the meny state with the level 1 state
+		}
+
+		if (mousePosition.x >= 709 && mousePosition.x <= 1196 && mousePosition.y >= 176 && mousePosition.y <= 272)
+		{
+			machine->RemoveState();  // Going back to the main menu
+
+			machine->AddState(StateRef(new Level4State));  // Replacing the meny state with the level 1 state
+		}
+		
+		//if (mousePosition.x >= 709 && mousePosition.x <= 1196 && mousePosition.y >= 272 && mousePosition.y <= 368)
+		//{
+		//	machine->RemoveState();  // Going back to the main menu
+
+		//	machine->AddState(StateRef(new Level5State));  // Replacing the meny state with the level 1 state
+		//}
+		
+		//if (mousePosition.x >= 111 && mousePosition.x <= 598 && mousePosition.y >= 368 && mousePosition.y <= 555)
+		//{
+		//	machine->RemoveState();  // Going back to the main menu
+
+		//	machine->AddState(StateRef(new Level6State));  // Replacing the meny state with the level 1 state
+		//}
+
+
+
 	}
 }
 void LevelSelectionState::Update(StateMachine* machine, float deltaTime)
